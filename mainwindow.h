1f7088dd01d4a55527c71e7f0edb43b5bec3b3a6
yolokimo768 <alinasr@cmail.carleton.ca>
2023-04-18 11:28:27 -0400

    working graph and stuff

--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,13 @@
 
 #include <QMainWindow>
 #include <QTimer>
+//new stuff for graph
+#include <QLabel>
+#include <QVector>
+#include <QtMultimedia>
+#include <QSystemTrayIcon>
+#include <QWidget>
+#include <QMediaPlayer>
 
 
 QT_BEGIN_NAMESPACE
@@ -24,10 +31,28 @@ private slots:
     void on_Uparrowmenu_clicked();
     void on_Selectbuttonsession_clicked();
     void updatetime();
+    void startNewSession();
+    void updateGraphData();
 
 private:
     Ui::MainWindow *ui;
     QTimer *timer;
     int elapsedSeconds;
+
+    QVector<double> getHRVData(){
+        QVector<double> hrvData;
+        for (int i=0; i<100; ++i){
+            hrvData.append(rand()%50+50);
+        }
+        return hrvData;
+    }
+    QVector<double> getTimeData(const QVector<double>& data, int sampleRate){
+        QVector<double> timeData;
+        for (int i = 0; i<data.size(); ++i){
+            timeData.append(static_cast<double>(i)/sampleRate);
+        }
+        return timeData;
+    }
+
 };
 #endif // MAINWINDOW_H
